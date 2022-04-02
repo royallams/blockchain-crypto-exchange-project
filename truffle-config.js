85a891f425b43ed269b0f668e66ba83de130bb79
royallams <royallams777@gmail.com>
2022-04-02 16:29:51 +0545

    Tetsing and implement transfer function

---
+++ truffle-config.js
@@ -0,0 +1,36 @@
+
+
+require('babel-register')
+require('babel-polyfill')
+require('dotenv').config();
+
+ 
+
+module.exports = {
+
+  networks: {
+     
+     development:{
+      host: "127.0.0.1",
+      port: 7545,
+      network_id:"*"
+     },
+  },
+
+
+contracts_directory: './src/contracts/',
+contracts_build_directory: './src/abis/',
+
+  
+compilers: {
+    solc: {
+       optimizer: {
+         enabled: false,
+         runs: 200
+    }
+       
+  }
+}
+}
+
+ 
